d0373c54722542460dda2efa61fef165a654b991
roberto-ramirez-flex <78037945+roberto-ramirez-flex@users.noreply.github.com>
2024-09-05 14:38:00 -0600

    Update variables.tf

--- tf-example/variables.tf
+++ tf-example/variables.tf
@@ -12,47 +12,38 @@ locals {
 
 variable "disk_info" {
   type        = list(any)
-  description = "Information for multiple disks"
   default = null
 }
 
 variable "aws_subnet" {
   type        = string
-  description = "subnet id"
 }
 
 variable "aws_security_group" {
   type        = string
-  description = "security group id"
 }
 
 variable "aws_family" {
   type        = string
-  description = "family type to build the ec2 instance"
 }
 
 variable "aws_region" {
   type        = string
-  description = "AWS Region"
 }
 
 variable "instance1" {
   type        = string
-  description = "Hostname instance"
 }
 
 variable "users_list" {
   type        = string
-  description = "Users list for access management"
   default     = "ansible"
 }
 
 variable "login_approle_role_id" {
   type = string
-  description = "vault approle id"
 }
 
 variable "login_approle_secret_id" {
   type = string
-  description = "vault approle secret id"
 }
